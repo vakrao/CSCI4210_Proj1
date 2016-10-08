0d9b1f68452242213977e5abed225d0e479b55af
dobbim <dobbim@rpi.edu>
2016-10-08 14:22:12 -0400

    Added initial script file

---
+++ proj1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+###############################################
+# CSCI-4210: Operating Systems                #
+# Project 1: Process Scheduling Simulator     #
+#                                             #
+# Matthew Dobbins (dobbim), Varun Rao (raov3) #
+# Due Thu 2016-Oct-13                         #
+###############################################
+
+if __name__ == '__main__':
+    print 'Hello, world!'
+    print 'Matt Dobbins, Varun Rao'
+    print 'CSCI-4210 Operating Systems'
+    print 'Project 1: Process Scheduling'
